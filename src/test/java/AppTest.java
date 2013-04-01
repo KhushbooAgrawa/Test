f4d2d5fb1f4100aa3f6e2b1966f5371ed35741d0
KhushbooAgrawal <Khushboo.Agrawal040389@gmail.com>
2013-04-01 11:28:40 +0530

    TEst

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest extends TestCase{
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
+	public void test(){
+		
+	}
+}
